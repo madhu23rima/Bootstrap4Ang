8e871a89c86a491a417cd3451d51ff01bf8fa64b
mail.madhurima@gmail.com <mail.madhurima@gmail.com>
2018-03-03 17:56:32 +1100

    angular with bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ContactComponent } from './contact/contact.component';
+import { Routes, RouterModule } from '@angular/router';
 
+const approutes: Routes =[
+  {path:'', redirectTo: '/home',pathMatch:'full'},
+  {path:'home', component: HomeComponent},
+  {path:'about', component: AboutComponent},
+  {path:'profile', component: ProfileComponent},
+  {path:'contact', component: ContactComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ProfileComponent,
+    ContactComponent
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(approutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
